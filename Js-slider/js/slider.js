b01fb3712b9ad684c92ddb2492d464f2ad75de11
sameer <sameerdeveloper05@gmail.com>
2023-04-30 15:26:37 +0530

    add comments for understood the javascript slider code

--- Js-slider/js/slider.js
+++ Js-slider/js/slider.js
@@ -1,17 +1,27 @@
 let imgWrapper = document.querySelectorAll(".slide");
 let SliderContainer = document.querySelector(".dots-container");
 let dotsSlider = document.getElementsByClassName("dot");
-let btn = document.getElementsByClassName('slide-btn');
-console.log(btn);
+let btn = document.getElementsByClassName("slide-btn");
+// This variable is use for move on slides
+// for Example:- Now counter value is zero when we click on next button 
+// then i use increment operator counter++ if i click two times on next 
+// button then the counter value is increase 0 to 2 then our image slide is going on second indexed
+
 var counter = 0;
+// This function for move previous slide;
 const prevSlide = () => {
   counter--;
   sliderImg();
 };
+
+// This function for move next image slide
 const nextSlide = () => {
   sliderImg();
   counter++;
 };
+
+// this function is used for perform images dots slider functionality
+
 const dotsSliderFunc = () => {
   for (var i = 0; i < imgWrapper.length; i++) {
     const SliderDots = document.createElement("div");
