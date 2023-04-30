e020b57284f609d77338a5625f93b3931fddf2a3
sameer <sameerdeveloper05@gmail.com>
2023-04-30 15:21:15 +0530

    Js Slider with next previous button and dots

--- Js-slider/js/slider.js
+++ Js-slider/js/slider.js
@@ -1,16 +1,42 @@
 let imgWrapper = document.querySelectorAll(".slide");
-let SliderContainer = document.querySelector('.dots-container');
-let dotsHtml = document.getElementsByClassName('dot');
+let SliderContainer = document.querySelector(".dots-container");
+let dotsSlider = document.getElementsByClassName("dot");
+let btn = document.getElementsByClassName('slide-btn');
+console.log(btn);
 var counter = 0;
 const prevSlide = () => {
-  sliderImg();
   counter--;
+  sliderImg();
 };
 const nextSlide = () => {
   sliderImg();
   counter++;
 };
+const dotsSliderFunc = () => {
+  for (var i = 0; i < imgWrapper.length; i++) {
+    const SliderDots = document.createElement("div");
+    SliderDots.classList.add("dot");
+    SliderContainer.appendChild(SliderDots);
+    SliderDots.setAttribute("data-index", i);
 
+    SliderDots.addEventListener("click", function (i) {
+      for (var k = 0; k < dotsSlider.length; k++) {
+        dotsSlider[k].classList.remove("active");
+      }
+      i.target.classList.add("active");
+      counter = this.dataset.index;
+      imgWrapper.forEach((imgSlides, index) => {
+        imgSlides.style.left = `${index * 100}%`;
+        if (counter == imgWrapper.length) {
+          counter = 0;
+        }
+
+        imgSlides.style.transform = `translateX(-${counter * 100}%)`;
+      });
+    });
+  }
+};
+dotsSliderFunc();
 const sliderImg = () => {
   imgWrapper.forEach((imgSlides, index) => {
     imgSlides.style.left = `${index * 100}%`;
@@ -20,10 +46,5 @@ const sliderImg = () => {
     imgSlides.style.transform = `translateX(-${counter * 100}%)`;
   });
 };
-for(var i=0;i<imgWrapper.length;i++){
-    const SliderDots = document.createElement("div");
-    SliderDots.classList.add("dot");
-    // console.log(SliderDots)
-    SliderContainer.appendChild(SliderDots);   
-}
-setInterval(nextSlide, 2000);
+
+// setInterval(nextSlide, 3000);
